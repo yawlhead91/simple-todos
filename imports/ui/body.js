6bde427f61094534856edf466b981b2e9e7b7e3e
yawlhead91 <alexincork91@gmail.com>
2016-09-30 19:31:31 +0100

    set private function

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
@@ -8,6 +9,7 @@ import './tasks.js';
 
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
 });
 
  
@@ -36,10 +38,7 @@ Template.body.events({
     const text = target.text.value;
  
     // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
+    Meteor.call('tasks.insert', text);
  
     // Clear form
     target.text.value = '';
